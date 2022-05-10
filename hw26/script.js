4f4b660561bbd807257f7e196d0633bbae2ac179
elisdance <lizarud2004@gmail.com>
2022-05-10 21:47:12 +0300

    hw26

---
+++ hw26/script.js
@@ -0,0 +1,13 @@
+var s1 = Number(0.1+0.2);
+var n=2;
+alert(s1 + n);
+
+let memory = prompt("Введіть обсяг пам'яті :" , ' ');
+alert('Кількість файлів на флешці : ' + Math.floor(memory/0.8) ) ;
+
+let sum = prompt ('Наявна сума' , ' ' );
+let chocolate = prompt ('Ціна за одну шоколадку', '');
+alert('Кількість шоколадок ' + Math.floor(money/chocolate) + ' шт здача ' + (money%chocolate) + ' грн' );
+
+var a = prompt('Введіть число' , ' ' );
+alert(('' + a).split('').reverse().join(''));
